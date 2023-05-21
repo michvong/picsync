0cad9b477cf78a20402d1b6b16f34cd5ec5b2cc9
Michelle Vong <mva30@sfu.ca>
2023-05-20 19:02:14 -0700

    Open photo gallery on button click

--- app/src/main/java/com/aws/picsync/activities/MainActivity.kt
+++ app/src/main/java/com/aws/picsync/activities/MainActivity.kt
@@ -10,8 +10,10 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.aws.picsync.ui.theme.PicsyncTheme
 import android.content.Intent
+import android.net.Uri
 import android.provider.MediaStore
 import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 
 import com.aws.picsync.R
 
@@ -23,7 +25,16 @@ class MainActivity : ComponentActivity() {
         val galleryButton = findViewById<Button>(R.id.galleryButton);
         galleryButton.setOnClickListener {
             val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            startActivity(galleryIntent);
+            resultLauncher.launch(galleryIntent);
         }
     }
+
+    private var resultLauncher = registerForActivityResult(StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            // There are no request codes
+            val data: Intent? = result.data
+            val imageUri: Uri? = data?.data
+        }
+    }
+
 }
