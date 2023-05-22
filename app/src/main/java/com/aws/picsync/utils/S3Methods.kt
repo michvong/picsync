d5f93500dfaa11d288f5dd2ea991dcdd14451974
Michelle Vong <mva30@sfu.ca>
2023-05-21 18:40:51 -0700

    Add location constraint for bucket configuration, replace constant values with environment variables

--- app/src/main/java/com/aws/picsync/utils/S3Methods.kt
+++ app/src/main/java/com/aws/picsync/utils/S3Methods.kt
@@ -1,6 +1,10 @@
 package com.aws.picsync.utils
 
+import io.github.cdimascio.dotenv.dotenv
+import aws.sdk.kotlin.runtime.auth.credentials.StaticCredentialsProvider
 import aws.sdk.kotlin.services.s3.S3Client
+import aws.sdk.kotlin.services.s3.model.BucketLocationConstraint
+import aws.sdk.kotlin.services.s3.model.CreateBucketConfiguration
 import aws.sdk.kotlin.services.s3.model.CreateBucketRequest
 import aws.sdk.kotlin.services.s3.model.HeadBucketRequest
 import aws.sdk.kotlin.services.s3.model.ListObjectsRequest
@@ -10,14 +14,30 @@ import java.io.File
 import java.lang.Exception
 
 class S3Methods {
-    suspend fun checkBucketExists(bucketName: String): Boolean {
+    private val dotenv = dotenv {
+        directory = "/assets"
+        filename = "env"
+    }
+    private fun getStatic() : StaticCredentialsProvider {
+        val staticCredentials = StaticCredentialsProvider {
+            accessKeyId = dotenv["AWS_ACCESS_KEY_ID"]
+            secretAccessKey = dotenv["AWS_SECRET_ACCESS_KEY"]
+            sessionToken = dotenv["AWS_SESSION_TOKEN"]
+        }
+        return staticCredentials
+    }
+    private suspend fun checkBucketExists(bucketName: String): Boolean {
         val request = HeadBucketRequest {
             bucket = bucketName
         }
 
         return try {
-            S3Client { region = "us-west-1" }.use { s3 ->
+            S3Client {
+                region = dotenv["REGION"]
+                credentialsProvider = getStatic()
+            }.use { s3 ->
                 s3.headBucket(request)
+
             }
             true
         } catch (e: Exception) {
@@ -26,11 +46,21 @@ class S3Methods {
         }
     }
     suspend fun createNewBucket(bucketName: String) {
-        val request = CreateBucketRequest {
-            bucket = bucketName
+        if (!checkBucketExists(bucketName)) {
+            return;
         }
 
-        S3Client { region = "us-west-1" }.use { s3 ->
+        val request = CreateBucketRequest {
+            bucket = bucketName
+            createBucketConfiguration = CreateBucketConfiguration {
+                locationConstraint = BucketLocationConstraint.fromValue(dotenv["REGION"])
+            }
+        }
+
+        S3Client {
+            region = dotenv["REGION"]
+            credentialsProvider = getStatic()
+        }.use { s3 ->
             s3.createBucket(request)
             println("$bucketName is ready")
         }
@@ -41,7 +71,7 @@ class S3Methods {
             bucket = bucketName
         }
 
-        S3Client { region = "us-west-1" }.use { s3 ->
+        S3Client { region = dotenv["REGION"] }.use { s3 ->
             val response = s3.listObjects(request)
             response.contents?.forEach { myObject ->
                 println("The name of the key is ${myObject.key}")
@@ -57,7 +87,7 @@ class S3Methods {
             body = File(objectPath).asByteStream()
         }
 
-        S3Client { region = "us-west-1" }.use { s3 ->
+        S3Client { region = dotenv["REGION"] }.use { s3 ->
             val response = s3.putObject(request)
             println("Tag information is ${response.eTag}")
         }
